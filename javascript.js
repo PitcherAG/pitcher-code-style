6d62b6afd4a58ee59bf34b0ad60aec49b4419fad
Onur Elibol <onur.elibol@pitcher.com>
2020-11-26 13:30:02 +0100

    add rules

---
+++ javascript.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'max-len': [
+      'error',
+      {
+        code: 160,
+        ignoreComments: true,
+        ignoreUrls: true
+      }
+    ],
+    'no-prototype-builtins': 1,
+    'no-empty': 1,
+    'no-var': 1,
+    'no-unused-vars': 2,
+    'prefer-arrow-callback': 1,
+    'arrow-parens': ['error', 'as-needed'],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false
+      }
+    ],
+    'space-before-function-paren': ['error', {
+      anonymous: 'never',
+      named: 'never',
+      asyncArrow: 'always'
+    }]
+  }
+}
