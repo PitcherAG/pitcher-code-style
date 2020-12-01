d42e9a2eaf5e4768b0b9183065d7e750cfdd9fe6
Onur Elibol <onur.elibol@pitcher.com>
2020-12-01 13:44:20 +0100

    additional rules & inherit from pitcher config

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
-/** 
+/**
 |--------------------------------------------------
 | JavaScript rules, inherits eslint:recommended
 |--------------------------------------------------
 **/
+
+const pitcherConfig = require('./pitcher.config')
 
 module.exports = {
   extends: ['eslint:recommended'],
@@ -10,10 +12,11 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    indent: ['error', pitcherConfig.indent],
     'max-len': [
       'error',
       {
-        code: 160,
+        code: pitcherConfig.printWidth,
         ignoreComments: true,
         ignoreUrls: true
       }
@@ -22,6 +25,9 @@ module.exports = {
     'no-empty': 1,
     'no-var': 1,
     'no-unused-vars': 2,
+    'object-curly-spacing': ['error', 'always'],
+    'space-in-parens': ['error', 'never'],
+    'array-bracket-spacing': ['error', 'never'],
     'prefer-arrow-callback': 1,
     'arrow-parens': ['error', 'as-needed'],
     'prefer-const': [
@@ -31,10 +37,13 @@ module.exports = {
         ignoreReadBeforeAssign: false
       }
     ],
-    'space-before-function-paren': ['error', {
-      anonymous: 'never',
-      named: 'never',
-      asyncArrow: 'always'
-    }]
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
   }
 }
