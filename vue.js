212f6da52e5a26879c90f8a94d412144d2af099b
Onur Elibol <onur.elibol@pitcher.com>
2020-12-01 13:45:21 +0100

    add prettier, inherit from pitcher config, re-order rules

--- vue.js
+++ vue.js
@@ -5,7 +5,9 @@
 **/
 
 // inherit js rules
-const js = require.resolve('./javascript');
+const js = require.resolve('./javascript')
+const pitcherConfig = require('./pitcher.config')
+const prettierConfig = require('./prettier.config')
 
 module.exports = {
   // declare globals
@@ -17,61 +19,70 @@ module.exports = {
     $ngettext: true,
     $t: true
   },
-  extends: ['plugin:vue/vue3-strongly-recommended', js],
+  extends: ['plugin:vue/vue3-strongly-recommended', js, '@vue/prettier'],
   rules: {
+    // Show prettier rules as error instead of warning
+    'prettier/prettier': ['error', prettierConfig],
+
+    /* Naming rules*/
     'vue/custom-event-name-casing': ['error'],
-    'vue/require-default-prop': 0,
-    'vue/require-prop-types': ['error'],
-    // Disabled error: this rule can't be used with Vue 2.x,
-    // conflicts with no-v-for-template-key rule
-    'vue/no-v-for-template-key-on-child': 0,
-    // Enable when we migrate to Vue 3.x
-    // 'vue/no-lone-template': ['error', {
-    //   ignoreAccessible: false
-    // }],
-    'vue/order-in-components': ['error'],
-    'vue/this-in-template': ['error', 'never'],
-    'vue/block-tag-newline': ['error', {
-      singleline: 'always'
-    }],
-    'vue/no-potential-component-option-typo': ['error'],
-    'vue/html-comment-content-spacing': ['error', 'always'],
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
-    'vue/no-duplicate-attr-inheritance': ['error'],
-    'vue/no-static-inline-styles': ['error', {
-      allowBinding: false
-    }],
-    'vue/no-unused-properties': ['error', {
-      groups: ['props', 'setup']
-    }],
-    'vue/padding-line-between-blocks': ['error', 'always'],
-    'vue/require-name-property': ['error'],
-    'vue/match-component-file-name': ['error', {
-      extensions: ['vue']
-    }],
     'vue/no-reserved-component-names': ['error'],
-    'vue/singleline-html-element-content-newline': 0,
-    'vue/max-attributes-per-line': [
+
+    /* Component rules */
+    'vue/require-default-prop': 0,
+    'vue/require-name-property': ['error'],
+    'vue/require-prop-types': ['error'],
+    'vue/no-unused-properties': [
       'error',
       {
-        singleline: 4,
-        multiline: {
-          max: 1,
-          allowFirstLine: true
-        }
+        groups: ['props', 'setup']
       }
     ],
-    'vue/html-closing-bracket-newline': ['error', {
-      singleline: 'never',
-      multiline: 'always'
-    }],
+    'vue/no-potential-component-option-typo': ['warn'],
+    // Disabled for now
+    // 'vue/match-component-file-name': ['error', {
+    //   extensions: ['vue']
+    // }],
+
+    /* Order rules */
+    'vue/order-in-components': ['error'],
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'CONDITIONALS',
+          'LIST_RENDERING',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          'UNIQUE',
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'CONTENT',
+          'EVENTS'
+        ],
+        alphabetical: false
+      }
+    ],
+
+    /* Template rules */
+    'vue/html-comment-content-spacing': ['error', 'always'],
+    'vue/no-duplicate-attr-inheritance': ['error'],
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/this-in-template': ['error', 'never'],
+    // Disabled rule: can't be used with Vue 2.x, conflicts with no-v-for-template-key rule
+    'vue/no-v-for-template-key-on-child': 0,
+    'vue/padding-line-between-blocks': ['error', 'always'],
+    'vue/no-template-shadow': 'error',
     'vue/max-len': [
       'warn',
       {
-        code: 160,
-        template: 160,
-        tabWidth: 2,
-        comments: 200,
+        code: pitcherConfig.printWidth,
+        template: pitcherConfig.printWidth,
+        tabWidth: pitcherConfig.indent,
+        comments: pitcherConfig.commentWidth,
         ignorePattern: '',
         ignoreComments: true,
         ignoreTrailingComments: true,
@@ -83,9 +94,20 @@ module.exports = {
         ignoreHTMLTextContents: false
       }
     ],
+    // Disabled completely, as this is colliding with Prettier
+    // 'vue/max-attributes-per-line': [
+    //   'error',
+    //   {
+    //     singleline: 4,
+    //     multiline: {
+    //       max: 1,
+    //       allowFirstLine: true
+    //     }
+    //   }
+    // ]
     'vue/html-indent': [
       'warn',
-      2,
+      pitcherConfig.indent,
       {
         attribute: 1,
         baseIndent: 1,
@@ -106,25 +128,28 @@ module.exports = {
         math: 'always'
       }
     ],
-    'vue/no-template-shadow': 'error',
-    'vue/attributes-order': [
+    'vue/html-closing-bracket-newline': [
       'error',
       {
-        order: [
-          'DEFINITION',
-          'CONDITIONALS',
-          'LIST_RENDERING',
-          'RENDER_MODIFIERS',
-          'GLOBAL',
-          'UNIQUE',
-          'TWO_WAY_BINDING',
-          'OTHER_DIRECTIVES',
-          'OTHER_ATTR',
-          'CONTENT',
-          'EVENTS'
-        ],
-        alphabetical: false
+        singleline: 'never',
+        multiline: 'always'
+      }
+    ],
+    'vue/block-tag-newline': [
+      'error',
+      {
+        singleline: 'always'
+      }
+    ],
+    'vue/no-static-inline-styles': [
+      'error',
+      {
+        allowBinding: true
       }
     ]
+    // Enable when we migrate to Vue 3.x
+    // 'vue/no-lone-template': ['error', {
+    //   ignoreAccessible: false
+    // }]
   }
 }
